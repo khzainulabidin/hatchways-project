f3a52c93ecb2a82f21bd7984dcc0f709943669ab
Zain Ul Abidin <khzainulabidin@gmail.com>
2021-06-17 17:10:14 +0500

    UI Bug fixed

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { Box } from "@material-ui/core";
 import { SenderBubble, OtherUserBubble } from "../ActiveChat";
 import moment from "moment";
@@ -6,16 +6,17 @@ import moment from "moment";
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
 
-  /*
-  * Reverses the messages array and renders it.
-  * Latest message will be on the bottom
-  * Smooth scroll behaviour is implemented which automatically takes user to latest message i.e to the bottom of page -
-  * - on selecting chat from the sidebar and on sending a new message
-  * */
+  useEffect(() => {
+    /*
+    * Smoothly scroll to the bottom of the page i.e to the latest message in the active chat
+    * */
+
+    window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+  }, [props])
 
   return (
     <Box>
-      {messages.reverse().map((message) => {
+      {messages.map((message) => {
         const time = moment(message.createdAt).format("h:mm");
 
         return message.senderId === userId ? (
